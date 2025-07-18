6ca089880de9b923ce7e2969f66112d9739d1b9b
hari9966-hari <ch7952464@gmail.com>
2025-07-18 14:44:51 +0530

    Add .next/static/chunks/src_app_admin_drive_[id]_page_tsx_a993527f._.js 1752830651375

---
+++ .next/static/chunks/src_app_admin_drive_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b664564f._.js",
+  "static/chunks/src_b7e95e96._.js"
+],
+    source: "dynamic"
+});
