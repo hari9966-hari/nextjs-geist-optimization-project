4b04f5dadf59dbd95478933e8eb34124e741bade
hari9966-hari <ch7952464@gmail.com>
2025-07-18 14:44:55 +0530

    Add .next/static/chunks/src_app_student_profile_page_tsx_a993527f._.js 1752830654653

---
+++ .next/static/chunks/src_app_student_profile_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d77bee5e._.js",
+  "static/chunks/node_modules_05084678._.js"
+],
+    source: "dynamic"
+});
